b61767e9a92c80d248308b03a94aa04a2bfe599c
jrdnc <jrdnblvn@gmail.com>
2020-09-18 20:29:27 -0500

    made clock show accurate time and made script dynamic

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-// initialize positions for each hand
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+function runClock() {
+    // get current time
+  var date = new Date();
+  console.log(date);
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-// get current timeout
-var date = new Date();
-console.log(date);
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+  // convert current time to degrees on circle ([num sec] * [total deg] / [total sec])
+  let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12); // make hr hand move gradually with min hand each minute
+  let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60); // make min hand move gradually with sec hand each second
+  let secPosition = sec * 360 / 60;
 
-// rotate each hand according to apporiate hand position
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+  // rotate each hand according to apporiate hand position
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+// run the runClock function every 1000 milliseconds (i.e. every second); makes script dynamic
+var inverval = setInterval(runClock, 1000);
